cd509671d50a620ca2c3eab8c35e76067a520f8c
Matheus Ribeiro Avila <ribeiroavilamatheus@gmail.com>
2025-08-13 19:37:52 -0300

    Projeto Stage 2

---
+++ src/main/java/com/playstore/webplaystore/WebplaystoreApplication.java
@@ -0,0 +1,13 @@
+package com.playstore.webplaystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebplaystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebplaystoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playstore/webplaystore/WebplaystoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playstore.webplaystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebplaystoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
