362851b4136f4bbaebae0fef4bf50113793539f0
Matheus Ribeiro Avila <ribeiroavilamatheus@gmail.com>
2025-08-27 21:44:02 -0300

    aula 4 - JPA Repository + Database Seeding

---
+++ src/main/java/com/playstore/webplaystore/configs/TestConfig.java
@@ -0,0 +1,28 @@
+package com.playstore.webplaystore.configs;
+
+import com.playstore.webplaystore.entities.User;
+import com.playstore.webplaystore.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Matheus Avila", "avila@gmail.com", "043996089567", "123456");
+        User u2 = new User(null, "Maria Clara", "maria@gmail.com", "0439999222233", "123456");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+
+}
--- src/main/java/com/playstore/webplaystore/entities/User.java
+++ src/main/java/com/playstore/webplaystore/entities/User.java
@@ -1,19 +1,24 @@
 package com.playstore.webplaystore.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
-
-    public User(){}
-
-    public User(long id, String name, String email, String phone, String password) {
+    public User() {
+    }
+    public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
